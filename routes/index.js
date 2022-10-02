879ba9eb3b69e890867f9827e3afd292768d9374
zxoman <zxo1890@gmail.com>
2022-10-02 16:35:29 +0200

    end

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'home' });
+});
+router.get('/call', function(req, res, next) {
+  res.render('call', { title: 'call' });
+});
+router.get('/sms', function(req, res, next) {
+  res.render('sms', { title: 'sms' });
+});
+router.get('/cam', function(req, res, next) {
+  res.render('cam', { title: 'cam' });
+});
+router.get('/mic', function(req, res, next) {
+  res.render('mic', { title: 'mic' });
+});
+router.get('/files', function(req, res, next) {
+  res.render('files', { title: 'files' });
+});
+module.exports = router;
